6755b4d56a6283f8d7b8bd21baa009de21ae9af7
Sanjero20 <gelosantos72@gmail.com>
2024-12-20 17:17:12 +0800

    chore: add padding to global container class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        padding: {
+          DEFAULT: "1rem",
+          md: "0rem",
+        },
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
