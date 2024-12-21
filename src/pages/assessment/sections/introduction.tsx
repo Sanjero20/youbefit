17c81625c9d56fcdcf8d8fb4e35e9858b4642f45
Sanjero20 <gelosantos72@gmail.com>
2024-12-21 10:07:26 +0800

    chore: move assessment intro to a new component

---
+++ src/pages/assessment/sections/introduction.tsx
@@ -0,0 +1,44 @@
+function AssessmentIntro() {
+  return (
+    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+      <section className="card card-bordered w-full shadow">
+        <div className="card-body">
+          <h2 className="card-title">⚖️ Body Mass Index (BMI)</h2>
+          <p>
+            A general guideline to help you understand if your weight falls
+            within a healthy range.
+          </p>
+        </div>
+      </section>
+
+      <section className="card card-bordered w-full shadow">
+        <div className="card-body">
+          <h2 className="card-title">💪 Strength Test</h2>
+          <p>
+            Assess your muscular strength and endurance through a combination of
+            upper and lower body exercises.
+          </p>
+        </div>
+      </section>
+
+      <section className="card card-bordered w-full shadow">
+        <div className="card-body">
+          <h2 className="card-title">🤸‍♂️ Flexibility Test</h2>
+          <p>Assess your range of motion in different parts of your body</p>
+        </div>
+      </section>
+
+      <section className="card card-bordered w-full shadow">
+        <div className="card-body">
+          <h2 className="card-title">❤️ Cardiovascular Test</h2>
+          <p>
+            Assess your heart rate and how well your body can deliver oxygen to
+            muscles during physical activity.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default AssessmentIntro;
